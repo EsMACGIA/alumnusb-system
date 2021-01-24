2f9a9ba253bd84b252751d80f3f897bdd8c01b8c
JeanPaulYazbek <jeanpaulyf98@hotmail.com>
2021-01-24 18:40:24 -0400

    Improved serializers

--- api/accounts/serializers.py
+++ api/accounts/serializers.py
@@ -29,18 +29,13 @@ class UserInformationSerializer(serializers.ModelSerializer):
  
     class Meta:
         model = User_information
-        fields = ('First_name', 'Middle_name', 'Last_name', 'Mailing_city', 'Mailing_state', 'USB_alumn', 'Codigo_Alumn_USB',
-        'Mailing_country', 'Email', 'Mobile', 'Cohorte', 'Birthdate', 'Age', 'Undergrad_degree', 'Graduate_degree', 
-        'Carnet', 'USB_undergrad_campus', 'Graduate_campus', 'Work_email', 'Workplace', 'Donor', 'Social_networks', 'Twitter_account', 
-        'Instagram_account', 'Picture')
-
-    
+        exclude = ['id']
+        read_only_fields = ['Email']
 
 class UserStatsSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model = User_information
-        fields = ('Email', 'Average_gift', 'Largest_gift', 'Smallest_gift', 'Total_gifts', 'Best_gift_year_total', 'Best_gift_year',
-        'Best_gift_year', 'First_gift_date', 'Last_gift_date', 'Total_number_of_gifts')
+        model = User_stats
+        fexclude = ['id']
     
       
